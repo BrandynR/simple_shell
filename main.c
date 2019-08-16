13dfd8f6730bfd02c92c07e58b1a7149e7120d8d
Osama Basheer <838@holbertonschool.com>
2019-08-16 20:44:27 +0000

    Add main file

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "shell.h"
+/**
+ * main - main function
+ * @argc: number of arguements
+ * @argv: arguement
+ * @envp: program’s environment
+ * Return:
+ */
+int main(int argc, char *argv[], char *envp[])
+{
+	loop = read line -> get_line
+		parse -> strtok
+		execute -> execue
+}
