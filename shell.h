16d336646bb2ce58e27a5fd7b573df257a15e3b5
Osama Basheer <838@holbertonschool.com>
2019-08-27 02:19:20 +0000

    Update header file

--- shell.h
+++ shell.h
@@ -1,13 +1,35 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <stddef.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+#include <stdlib.h>
+#include <signal.h>
+extern char **environ;
+extern int dircount;
+#define DELIM " \n\t"
 
-#endif /* SHELL_H */
+void env(char **env);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+void prompt(void);
+char *get_line(void);
+char **split_line(char *line);
+char *get_env(char **env);
+char *pathCat(char *dir, char *av);
+char **dirTok(char **env);
+
+void looper(char **env);
+char *checkPath(char **dir, char *command);
+int execute(char *fullPath, char **command);
+int exitme(char **command);
+int cd(char **command);
+int printenv(char **command);
+int checkBuiltins(char *combine, char **command);
+void  handler(int sig);
+#endif
