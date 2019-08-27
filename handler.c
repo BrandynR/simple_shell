7567acf60b37b6ce4ea8a7fbea744948f62e8a15
BrandynR <BrandynR@github.com>
2019-08-27 06:44:21 +0000

    Fixed Betty errors

--- handler.c
+++ handler.c
@@ -1,8 +1,13 @@
 #include "shell.h"
+/**
+ *handler - handles signal prints new line
+ *@sig: signal
+ *Return: void
+ */
 void  handler(int sig)
 {
 	signal(sig, SIG_IGN);
-	write (1, "\n", 1);
+	write(1, "\n", 1);
 	prompt();
 	signal(SIGINT, handler);
 }
