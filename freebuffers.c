b6ecdf5de81b22c8d134e844f75f57206b472eb3
BrandynR <BrandynR@github.com>
2019-08-30 22:44:59 +0000

    Fixed whitspace

--- freebuffers.c
+++ freebuffers.c
@@ -7,29 +7,30 @@
  */
 void buffers1(char *line, char **command)
 {
-        static char *l;
-        static char **cmd;
-        if (!line && !command)
-        {
-                {
-                        if (l)
-                                free(l);
-                        if (cmd)
-                                free(cmd);
-                }
-                if (line)
-                {
-                        if (l)
-                                free(l);
-                        l = line;
-                }
-                if (command)
-                {
-                        if (cmd)
-                                free(cmd);
-                        cmd = command;
-                }
-        }
+	static char *l;
+	static char **cmd;
+
+	if (!line && !command)
+	{
+		{
+			if (l)
+				free(l);
+			if (cmd)
+				free(cmd);
+		}
+		if (line)
+		{
+			if (l)
+				free(l);
+			l = line;
+		}
+		if (command)
+		{
+			if (cmd)
+				free(cmd);
+			cmd = command;
+		}
+	}
 }
 
 /**
@@ -40,28 +41,28 @@ void buffers1(char *line, char **command)
  */
 void buffers2(char **dir, char *combine)
 {
-        static char **di;
-        static char *co;
+	static char **di;
+	static char *co;
 
-        if (!dir && !combine)
-        {
-                if (di)
-                        free(di);
-                if (co)
-                        free(co);
-        }
-        if (dir)
-        {
-                if (di)
-                        free(di);
-                di = dir;
-        }
-        if (combine)
-        {
-                if (co)
-                        free(co);
-                co = combine;
-        }
+	if (!dir && !combine)
+	{
+		if (di)
+			free(di);
+		if (co)
+			free(co);
+	}
+	if (dir)
+	{
+		if (di)
+			free(di);
+		di = dir;
+	}
+	if (combine)
+	{
+		if (co)
+			free(co);
+		co = combine;
+	}
 }
 /**
  *buffers3 - frees token and buffer
@@ -71,28 +72,28 @@ void buffers2(char **dir, char *combine)
  */
 void buffers3(char **tokens, char *buf)
 {
-        static char **tokn;
-        static char *bu;
+	static char **tokn;
+	static char *bu;
 
-        if (!tokens && !buf)
-        {
-                if (tokn)
-                        free(tokn);
-                if (bu)
-                        free(bu);
-        }
-        if (tokens)
-        {
-                if (tokn)
-                        free(tokn);
-                tokn = tokens;
-        }
-        if (buf)
-        {
-                if (bu)
-                        free(bu);
-                bu = buf;
-        }
+	if (!tokens && !buf)
+	{
+		if (tokn)
+			free(tokn);
+		if (bu)
+			free(bu);
+	}
+	if (tokens)
+	{
+		if (tokn)
+			free(tokn);
+		tokn = tokens;
+	}
+	if (buf)
+	{
+		if (bu)
+			free(bu);
+		bu = buf;
+	}
 }
 /**
  *buffers4 - frees tok and buf2
@@ -102,28 +103,28 @@ void buffers3(char **tokens, char *buf)
  */
 void buffers4(char **tok, char *buf2)
 {
-        static char **to;
-        static char *b2;
+	static char **to;
+	static char *b2;
 
-        if (!tok && !buf2)
-        {
-                if (to)
-                        free(to);
-                if (b2)
-                        free(b2);
-        }
-        if (tok)
-        {
-                if (to)
-                        free(to);
-                to = tok;
-        }
-        if (buf2)
-        {
-                if (b2)
-                        free(b2);
-                b2 = buf2;
-        }
+	if (!tok && !buf2)
+	{
+		if (to)
+			free(to);
+		if (b2)
+			free(b2);
+	}
+	if (tok)
+	{
+		if (to)
+			free(to);
+		to = tok;
+	}
+	if (buf2)
+	{
+		if (b2)
+			free(b2);
+		b2 = buf2;
+	}
 }
 
 /**
@@ -133,17 +134,17 @@ void buffers4(char **tok, char *buf2)
  */
 void buffers5(char *dup)
 {
-        static char *du;
+	static char *du;
 
-        if (!dup)
-        {
-                if (du)
-                        free(du);
-        }
-        if (dup)
-        {
-                if (du)
-                        free(du);
-                du = dup;
-        }
+	if (!dup)
+	{
+		if (du)
+			free(du);
+	}
+	if (dup)
+	{
+		if (du)
+			free(du);
+		du = dup;
+	}
 }
